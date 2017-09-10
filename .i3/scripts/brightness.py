98b4b1d552b147b830f100ca8b535b0730e78c0d
gabrwagn <gabrwagn@gmail.com>
2017-09-10 13:50:41 +0200

    Add better compton startup and invert selected

--- .i3/scripts/brightness.py
+++ .i3/scripts/brightness.py
@@ -1,21 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import os
 
-output = ' '
-
+output = ' '
+color = ''
 path = os.path.join('/sys', 'class', 'backlight', 'intel_backlight')
 brightness = 0
 max_brightness = 0
 
-with open(os.path.join(path, 'brightness'), 'r') as fb:
-    brightness = fb.readline()
-
 with open(os.path.join(path, 'max_brightness'), 'r') as fb:
-    max_brightness = fb.readline()
+    max_brightness = float(fb.readline())
 
-percent = float(brightness)/float(max_brightness)
+with open(os.path.join(path, 'brightness'), 'r') as fb:
+    brightness = min(float(fb.readline()), max_brightness / 2)
 
-output += str(int(percent * 100)) + '%'
+percent = int(100 * float(brightness)/float(max_brightness))
+
+output += 'l'*(int(10 * percent / 100))
 
 print(output)
+print(output)
+print(color)
